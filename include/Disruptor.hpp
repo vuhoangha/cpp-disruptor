1b88e8da6fbf80ceba6a93d22443c89c2225dcdf
Kendrick <vuhoangha100995@gmail.com>
2025-05-23 17:53:23 +0700

    add disruptor

--- include/Disruptor.hpp
+++ include/Disruptor.hpp
@@ -1,7 +1,12 @@
 #pragma once
+#include "RingBuffer.hpp"
 
 namespace disruptor {
     template<typename T, size_t N>
     class Disruptor {
+        RingBuffer<T> ringBuffer;
+
+        tiếp tục nghĩ cách tạo thread factory hoặc tạo mặc định đơn giản cho dễ
+
     };
 }
